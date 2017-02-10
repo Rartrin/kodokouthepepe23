0d5af6777fa41c8a47d5ad00598691c95dc44ef0
Rartrin <polliwhirl@gmail.com>
2017-02-10 15:53:32 -0800

    Added SpecialistItem base

---
+++ Promethium/Items/SpecialistItem.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Promethium;
+
+namespace Promethium.Items
+{
+	public abstract class SpecialistItem:ModItem
+	{
+		public byte specialistID;
+		
+		public static bool CheckSpecialistID(Player player)
+		{
+			return player.GetModPlayer<CCMPlayer>(mod).specialistID==specialistID;
+		}
+	}
+}
